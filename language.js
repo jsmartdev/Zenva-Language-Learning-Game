b1e8215882f81882b6c89172bbde21954c5a5052
Jesse Smart <jsmartdev@protonmail.com>
2021-02-27 22:31:56 -0500

    Complete app structure part 1

--- language.js
+++ language.js
@@ -3,33 +3,33 @@
 let question = {
     title: "gato",
     choices: ["dog", "cat", "bird", "fish"],
-    answer: 1
+    correct: 1
 };
 
-function showQuestion(q) {
-    // show question title
-    let titelDiv = document.getElementById("title");
-    titelDiv.textContent = question.title;
+// function for starting the application
+function start() {
     // show possible answers
     let answers = document.querySelectorAll(".choice");
     answers.forEach(function(element, index) {
-        element.textContent = q.choices[index];
         element.addEventListener("click", function() {
             //check correct answer
-            if(q.correctAnswer = index) {
-                console.log("Congratulations, you have the correct answer!")
-            }
-            else {
-                console.log("Sorry, you are incorrect.")
-            }
-    })
-});
+            console.log("Check for correct answer!")
+        })
+    });
+    // show first question
+    showQuestion(question);
+};
+
+// function for showing the question
+function showQuestion(q) {
+    // show question title
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = q.title; 
+    // show alternatives
+    let answers = document.querySelectorAll('.choice');
+    answers.forEach(function(element, index){
+    element.textContent = q.alternatives[index];
+    });
 }
 
-showQuestion(question);
-
-let btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-    console.log("clicked!");
-});
+start();
