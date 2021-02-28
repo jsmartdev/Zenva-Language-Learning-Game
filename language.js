039d0559e7bfd79b7e959512b6a07b4ebacc987f
Jesse Smart <jsmartdev@protonmail.com>
2021-02-28 12:13:21 -0500

    Complete Show a Question

--- language.js
+++ language.js
@@ -1,86 +1,18 @@
 /* eslint-disable */
 
-let question = {
+question = {
     title: "gato",
-    choices: ["dog", "cat", "bird", "fish"],
-    correct: 1
+    alternatives: ["dog", "cat", "bird", "fish"],
+    correctAnswer: 1
 };
 
-let questions = [
-    {
-    title: "gato",
-    choices: ["dog", "cat", "bird", "fish"],
-    correct: 1
-    },
-    {
-    title: "ave",
-    choices: ["mouse", "hamster", "lizard", "bird"],
-    correct: 3
-    },
-    {
-    title: "rata",
-    choices: ["cat", "fish", "rat", "shark"],
-    correct: 2
-    },
-    {
-    title: "mosca",
-    choices: ["fly", "puma", "fish", "dog"],
-    correct: 0
+function showQuestion(q) {
+    // select DOM element
+
+    let titleDiv = document.getElementById("title");
+
+    // modify DOM element
+
+    titleDiv.textContent = q.title;
 }
-]
 
-let app = {
-    start: function(){
-
-        this.currentPositon = 0;
-        // show possible answers
-        let answers = document.querySelectorAll(".choice");
-        answers.forEach(function(element, index) {
-            element.addEventListener("click", function() {
-            //check correct answer
-            console.log("Check for correct answer!")
-            })
-        });
-    // show first question
-    this.showQuestion(questions[this.currentPosition]);
-    },
-
-    showQuestion: function(question) {
-        //keep track of the current question
-        this.currentQuestion = q;
-        // show question title
-        let titleDiv = document.getElementById('title');
-        titleDiv.textContent = question.title; 
-        // show alternatives
-        let answers = document.querySelectorAll('.choice');
-        answers.forEach(function(element, index){
-        element.textContent = question.choices[index];
-        });
-    },
-
-    checkAnswer: function(userSelected) {
-        if(this.currentQuestion.correctAnswer == userSelected)  {
-            //correct
-            console.log("Correct!");
-        }
-        else {
-            //not correct
-            console.log("Wrong!");
-        }
-
-        //increase position
-        this.increasePosition();
-        
-        //show next question
-        this.showQuestion(questions[this.currentPosition])
-    },
-
-    increasePosition: function() {
-        this.currentPosition++;
-        if(this.currentPosition == questions.length) {
-            this.currentPosition = 0;
-        }
-    }
-};
-
-app.start();
