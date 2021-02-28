fbbe67aadf7a69ea4764fe5598ac0ae9c566ebe4
Jesse Smart <jsmartdev@protonmail.com>
2021-02-27 20:08:34 -0500

    Complete show question lesson

--- language.js
+++ language.js
@@ -5,3 +5,14 @@ let question = {
     choices: ["dog", "cat", "bird", "fish"],
     answer: 1
 };
+
+function showQuestion(q) {
+let titelDiv = document.getElementById("title");
+titelDiv.textContent = question.title;
+let answers = document.querySelectorAll(".choice");
+answers.forEach(function(element, index) {
+    element.textContent = q.choices[index];
+});
+}
+
+showQuestion(question);
