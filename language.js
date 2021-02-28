15db905511b8eb2f713f7a6aea3c5323e4550cae
Jesse Smart <jsmartdev@protonmail.com>
2021-02-27 21:26:21 -0500

    Complete handle user input lesson

--- language.js
+++ language.js
@@ -7,12 +7,29 @@ let question = {
 };
 
 function showQuestion(q) {
-let titelDiv = document.getElementById("title");
-titelDiv.textContent = question.title;
-let answers = document.querySelectorAll(".choice");
-answers.forEach(function(element, index) {
-    element.textContent = q.choices[index];
+    // show question title
+    let titelDiv = document.getElementById("title");
+    titelDiv.textContent = question.title;
+    // show possible answers
+    let answers = document.querySelectorAll(".choice");
+    answers.forEach(function(element, index) {
+        element.textContent = q.choices[index];
+        element.addEventListener("click", function() {
+            //check correct answer
+            if(q.correctAnswer = index) {
+                console.log("Congratulations, you have the correct answer!")
+            }
+            else {
+                console.log("Sorry, you are incorrect.")
+            }
+    })
 });
 }
 
 showQuestion(question);
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    console.log("clicked!");
+});
